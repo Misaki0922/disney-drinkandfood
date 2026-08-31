c4e6ca85ecc1b935507bdcb7cb9b74e7ad9b7c58
Misaki0922 <sklvme@gmail.com>
2026-08-31 14:49:48 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,41 @@
+/* キャッシュ名を変えると、次に開いたときに新しい内容へ入れ替わります。
+   中身を更新したら日付の部分を書き換えてください。 */
+const CACHE = "freedrink-2026-09-01";
+
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.webmanifest",
+  "./apple-touch-icon.png",
+  "./icon-192.png",
+  "./icon-512.png",
+  "./icon-512-maskable.png",
+  "./favicon.png"
+];
+
+self.addEventListener("install", e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
+});
+
+self.addEventListener("activate", e => {
+  e.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
+
+/* まずネットワークを試し、圏外ならキャッシュを返します。
+   パーク内で電波が悪くても開けるようにするためです。 */
+self.addEventListener("fetch", e => {
+  if (e.request.method !== "GET") return;
+  e.respondWith(
+    fetch(e.request)
+      .then(res => {
+        const copy = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, copy));
+        return res;
+      })
+      .catch(() => caches.match(e.request).then(r => r || caches.match("./index.html")))
+  );
+});
